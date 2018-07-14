48db1336fa252c66053f810857b94d20605f4433
geetest <1069664125@qq.com>
2018-07-15 00:27:06 +0800

    commit

---
+++ jni/Application.mk
@@ -0,0 +1,5 @@
+NDK_TOOLCHAIN_VERSION=4.9
+APP_PROJECT_PATH := /Users/gunaonian/AndroidStudioProjects/FairyOpenssl
+APP_BUILD_SCRIPT := $(APP_PROJECT_PATH)/Android.mk
+APP_PLATFORM := android-14
+APP_ABI  := armeabi armeabi-v7a arm64-v8a x86 x86_64
